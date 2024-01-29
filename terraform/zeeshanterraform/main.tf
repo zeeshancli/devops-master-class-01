56fbcce469652c714bb4ad799aa75af8e914cedd
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 19:03:13 +0530

    iamuser

--- terraform/zeeshanterraform/main.tf
+++ terraform/zeeshanterraform/main.tf
@@ -32,6 +32,10 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   }
 }
 
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
+
 output "my_s3_bucket_versioning" {
     value = aws_s3_bucket.my_s3_bucket_zeeshan_786_01.versioning[0].enabled
 }
