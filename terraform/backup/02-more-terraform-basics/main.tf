71e131c34750d5c1fb82ef53502955583f6f0bec
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 20:00:01 +0530

    needed

--- terraform/backup/02-more-terraform-basics/main.tf
+++ terraform/backup/02-more-terraform-basics/main.tf
@@ -1,14 +1,28 @@
-variable "iam_user_name_prefix" {
-  type    = string #any, number, bool, list, map, set, object, tuple
-  default = "my_iam_user"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
-
+ 
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
-  //version = "~> 2.46" (No longer necessary)
+  region = "us-east-1"
+  # VERSION IS NOT NEEDED HERE
 }
 
-resource "aws_iam_user" "my_iam_users" {
-  count = 1
-  name  = "${var.iam_user_name_prefix}_${count.index}"
+
+
+resource "aws_s3_bucket" "your_s3_buckets" {
+    count = 3
+    bucket = "${var.my-s3-bucket-prefix}_${count.index}"
+ #   versioning {
+ #       enabled = true
+ #   }
 }
+
+
+
+
