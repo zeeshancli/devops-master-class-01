a2c7daa63b14a556c0b56a826cf5a8cb19102917
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 23:59:42 +0530

    space

--- terraform/backup/04-maps/main2.tf
+++ terraform/backup/04-maps/main2.tf
@@ -1,8 +1,8 @@
 variable "usersfor"{
     default = {
-        zeeshan: {country: "Netherlands" , freinds: "US"}
-        kareem: {country: "canada" , friends: "noone"}
-        talib:  {country: "india" , friends: "veryless"}
+        zeeshan: {country : "Netherlands" , freinds : "US"}
+        kareem: {country : "canada" , friends : "noone"}
+        talib:  {country : "india" , friends : "veryless"}
     }
 }
 
@@ -11,7 +11,7 @@ resource "aws_iam_user" "my_iam_second_users" {
     for_each = var.usersfor
     name = each.key
     tags = {
-        country: each.value.country
-        friends: each.value.friends
+        country : each.value.country
+        friends : each.value.friends
     }
 }
