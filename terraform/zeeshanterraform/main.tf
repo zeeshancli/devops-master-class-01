8089da21b07934fd81acaeef73357a1a07b1d507
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 18:52:10 +0530

    needed

--- terraform/zeeshanterraform/main.tf
+++ terraform/zeeshanterraform/main.tf
@@ -15,23 +15,23 @@ provider "aws" {
 
 # plan
 resource "aws_s3_bucket" "my_s3_bucket_zeeshan_786" {
-    bucket = "my-s3-bucket-zeeshan-786"
+    bucket = "my-s3-bucket-zeeshan-786_01"
 }
 
 resource "aws_s3_bucket" "my_s3_bucket_zeeshan_786" {
-    bucket = "my-s3-bucket-zeeshan-786"
+    bucket = "my-s3-bucket-zeeshan-786_01"
  #   versioning {
  #       enabled = true
  #   }
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.my_s3_bucket_zeeshan_786.id
+  bucket = aws_s3_bucket.my_s3_bucket_zeeshan_786_01.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 output "my_s3_bucket_versioning" {
-    value = aws_s3_bucket.my_s3_bucket_zeeshan_786.versioning[0].enabled
+    value = aws_s3_bucket.my_s3_bucket_zeeshan_786_01.versioning[0].enabled
 }
