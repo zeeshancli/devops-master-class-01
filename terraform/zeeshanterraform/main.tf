11e52f9986342caf644ad20f078ac6a5b52f00b3
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 18:48:20 +0530

    output

--- terraform/zeeshanterraform/main.tf
+++ terraform/zeeshanterraform/main.tf
@@ -18,9 +18,20 @@ resource "aws_s3_bucket" "my_s3_bucket_zeeshan_786" {
     bucket = "my-s3-bucket-zeeshan-786"
 }
 
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-in28minutes-rangak-002"
+ #   versioning {
+ #       enabled = true
+ #   }
+}
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.my_s3_bucket_zeeshan_786.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+output "my_s3_bucket_versioning" {
+    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
 }
