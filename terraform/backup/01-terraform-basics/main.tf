191e1f262ab729a68aba47b2db8cf258c7393f28
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 19:21:32 +0530

    commit

--- terraform/backup/01-terraform-basics/main.tf
+++ terraform/backup/01-terraform-basics/main.tf
@@ -1,11 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+ 
 provider "aws" {
-    region = "us-east-1"
-    //version = "~> 2.46" (No longer necessary)
+    region = "us-east-1"   
 }
 
 # plan - execute 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-s3-bucket-in28minutes-rangak-002"
+    bucket = "my-s3-bucket-in28minutes-rangak-zeehan"
  #   versioning {
  #       enabled = true
  #   }
@@ -18,7 +26,3 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   }
 }
 
-
-resource "aws_iam_user" "my_iam_user" {
-    name = "my_iam_user_abc_updated"
-}
