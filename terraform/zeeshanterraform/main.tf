a3da8a167e777da7fa988e9b630b22b50410c961
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 19:07:44 +0530

    -

--- terraform/zeeshanterraform/main.tf
+++ terraform/zeeshanterraform/main.tf
@@ -15,11 +15,11 @@ provider "aws" {
 
 # plan
 resource "aws_s3_bucket" "my_s3_bucket_zeeshan_786_01" {
-    bucket = "my-s3-bucket-zeeshan-786_01"
+    bucket = "my-s3-bucket-zeeshan-786-01"
 }
 
 resource "aws_s3_bucket" "my_s3_bucket_zeeshan_786_01" {
-    bucket = "my-s3-bucket-zeeshan-786_01"
+    bucket = "my-s3-bucket-zeeshan-786-01"
  #   versioning {
  #       enabled = true
  #   }
