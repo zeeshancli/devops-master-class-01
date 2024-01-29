bb0da354298da3fbb2cfe135db3cb7b079f27120
zeeshancli <102648128+zeeshancli@users.noreply.github.com>
2024-01-29 23:53:48 +0530

    NEW

---
+++ terraform/backup/04-maps/main2.tf
@@ -0,0 +1,20 @@
+varriables "users"{
+    defaults = {
+        zeeshan: {country: "Netherlands" , freinds: "US"}
+        kareem: {country: "canada" , friends: "noone"}
+        talib:  {country: "india" , friends: "veryless"}
+    }
+}
+provider "aws" {
+  region  = "us-east-1"
+  //version = "~> 2.46" (No longer necessary)
+}
+
+resource "aws_iam_user" "my_iam_users" {
+    for_each = var.users
+    name = each.key
+    tags = {
+        country: each.value.country
+        friends: each.value.friends
+    }
+}
